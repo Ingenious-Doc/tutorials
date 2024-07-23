67d511f9f83c1cb2dee612d8082ffa86b5620eef
Ingenious-Doc <51190860+Ingenious-Doc@users.noreply.github.com>
2024-07-23 09:41:17 +0400

    Update sale_managment.py by modifying the env variable to be correct

--- import_export/models/sale_managment.py
+++ import_export/models/sale_managment.py
@@ -4,12 +4,17 @@ from odoo import models, api, fields,_
 class sale_order_line(models.Model):
     _inherit="sale.order.line"
     recent_sale_order_line=fields.Many2one(comodel_name='sale.order.line',compute='_last_sale_order',store=True)
-   # recent_sale_order_date=fields.Date(related=recent_sale_order_line.order_id.date)
+    recent_sale_order_date=fields.Date(related=recent_sale_order_line.order_id.date)
     last_price=fields.Float(related=recent_sale_order_line.price_unit)
     
 
     def _last_sale_order(self):
-        self.recent_sale_order_line=env['sale.order.line'].search([],order='create_date desc',limit=1)
+        last_sale_order=self.env['sale.order.line'].sudo().search([],order='create_date desc',limit=1)
+        for record in self:
+            if last_sale_order:
+                record.recent_sale_order_line=last_sale_order.id
+            else:
+                record.recent_sale_order_line=False
         
 
 
